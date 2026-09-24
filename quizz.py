722d36eb102ec69e4a4684ce17f85660d56268d4
cahponto <ca.plus@outlook.com>
2026-09-24 17:25:24 -0300

    Corrige textos, formatação das perguntas e trata espaços na resposta

--- quizz.py
+++ quizz.py
@@ -1,32 +1,44 @@
 pontos = 0
 print ("=== QUIZ DE CONSCIENTIZAÇÃO EM SEGURANÇA ===")
-resposta1 = input ("Você recebeu um e-mail da diretoria pedindo para clicar em um link urgente de troca de senha. O que você faz?\n a) Clicar\n b) Reportar ao time de TI\n c) Só ignorar o e-mail\n").lower()
+resposta1 = input ("""Você recebeu um e-mail da diretoria pedindo para clicar em um link urgente de troca de senha. O que você faz?
+a) Clicar
+b) Reportar ao time de TI
+c) Só ignorar o e-mail 
+Reposta: """).strip().lower()
 if resposta1 == "b":
     print("Acertou!")
     pontos = pontos + 1 #Ganha 1 ponto
 else:
     print ("Errou!")
 
-resposta2 = input ("Você encontra um pendrive perdido no estacionamento ou na recepção da empresa. O que faz?\n a)Conectar no computador para ver de quem é\n b)Deixar onde está\n c)Entregar no setor de TI ou Segurança\n").lower()
+resposta2 = input ("""Você encontra um pendrive perdido no estacionamento ou na recepção da empresa. O que faz?
+a) Conectar no computador para ver de quem é
+b) Deixar onde está
+c) Entregar no setor de TI ou Segurança 
+Resposta: """).strip().lower()
 
 if resposta2 == "c":
     print("Acertou!")
     pontos = pontos + 1 #Ganha 1 ponto
 else: 
-    print ("Errou! Tente novamente")
+    print ("Errou!")
 
-resposta3 = input ("Um colega do seu setor pede sua senha do sistema para fechar um relatório urgente enquanto você está no almoço. Você?\n a) Nega o acesso e avisa que senhas são pessoais e intransferíveis\n b)Passa a senha, afinal é sua colegal de equipe\n c)Anota em um papel e deixa na mela dela\n").lower()
+resposta3 = input ("""Um colega do seu setor pede sua senha do sistema para fechar um relatório urgente enquanto você está no almoço. Você? 
+a) Nega o acesso e avisa que senhas são pessoais e intransferíveis
+b) Passa a senha, afinal é sua colega de equipe
+c) Anota em um papel e deixa na mesa dela 
+Resposta: """).strip().lower()
 if resposta3 == "a":
     print ("Acertou!")
     pontos = pontos + 1 #Ganha 1 ponto
 else:
     print ("Errou!")
 
-print ("=== FIM DO QUIZZ! ===")
+print ("=== FIM DO QUIZ! ===")
 print (f"Você acertou {pontos} de 3 perguntas")
 if pontos == 3:
     print ("Arrasou! Gabaritou!")
 elif pontos == 2:
-     print ("Tá tudo bem, errar faz parte...")
+    print ("Tá tudo bem, errar faz parte...")
 else:
     print ("Hmm... melhor tentar novamente, não?")
